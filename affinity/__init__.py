b1337e7fa55b06dc32d91a492f94e7a240af3b2e
Davide Loconte <davide.loconte@hotmail.it>
2022-05-13 18:32:14 +0200

    Add image slicer

--- affinity/__init__.py
+++ affinity/__init__.py
@@ -1,44 +1,60 @@
-from torch import nn
+from functools import lru_cache
+from math import ceil, floor
+
+from torch import eq, nn, permute, reshape
+from torchvision.transforms import functional as F
+
+class AffinitySlicer(nn.Module):
+    
+    def __init__(self, size = 60) -> None:
+       super().__init__()
+       self.size = size
+
+    def forward(self, x):
+        x = F.pad(x, self.size_to_padding(x.size))
+        x = x.unfold(-2, self.size, self.size).unfold(-2, self.size, self.size)
+        image_axis = [-4,-3,-5,-2,-1]
+        other_axis = [x for x in range(len(x.shape) - 5)] 
+        x = permute(x, other_axis + image_axis)
+        return x
+    
+    def location(self, img, r, c):
+        pad = self.size_to_padding(img.size)
+        x = (r * self.size) - pad[0]
+        y = (c * self.size) - pad[1]
+        return x, y 
+
+    @lru_cache(maxsize=8)
+    def size_to_padding(self, size):
+        return (
+            floor((self.size - size(-1) % self.size) / 2) if size(-1) % self.size != 0 else 0,
+            floor((self.size - size(-2) % self.size) / 2) if size(-2) % self.size != 0 else 0, # Top
+            ceil((self.size - size(-1) % self.size) / 2) if size(-1) % self.size != 0 else 0, # Right
+            ceil((self.size - size(-2) % self.size) / 2) if size(-2) % self.size != 0 else 0  # Top
+        )
+    
+    def __hash__(self) -> int:
+        return super().__hash__()
+
+    def __eq__(self, __o: object) -> bool:
+        return self.size == __o.size
+        
+        
 
 class AffinityNetwork(nn.Module):
-
-    # Convolutional layers
-    KERNEL_SIZE = 5
-    INPUT_CHANNELS = 3 # RGB
-    LEVEL_1_CHANNELS = 6
-    LEVEL_2_CHANNELS = 16
-    LEVEL_3_CHANNELS = 32
-
-    # Linear levels
-    INPUT_LINEAR = LEVEL_3_CHANNELS * (KERNEL_SIZE ** 2)
-    # Increase to add features to the linear network
-    LINEAR_1 = 120
-    LINEAR_2 = 84
-    LINEAR_3 = 1 # Output: confidence
-
-    # Pooling size between conv layers
-    POOL_SIZE = 2 # Increase to decrease features to the network
 
     """Small neural network for feature detection in images"""
     def __init__(self):
         super(AffinityNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(self.INPUT_CHANNELS, self.LEVEL_1_CHANNELS, self.KERNEL_SIZE)
-        self.conv2 = nn.Conv2d(self.LEVEL_1_CHANNELS, self.LEVEL_2_CHANNELS, self.KERNEL_SIZE)
-        self.conv3 = nn.Conv2d(self.LEVEL_2_CHANNELS, self.LEVEL_3_CHANNELS, self.KERNEL_SIZE)
-        self.linear1 = nn.Linear(self.INPUT_LINEAR, self.LINEAR_1)# 5x5 from image dimension
-        self.linear2 = nn.Linear(self.LINEAR_1, self.LINEAR_2)
-        self.linear3 = nn.Linear(self.LINEAR_2, self.LINEAR_3)
+        # Input tensor shape
+        # [B, Ch, W, H]
+        self.slicer = AffinitySlicer(size=60)
+        # Input tensor shape after slicing
+        # [B, R, Co, Ch, W, H]
 
-        self.pool = nn.MaxPool2d((self.POOL_SIZE, self.POOL_SIZE))
+        # Output tensor shape
+        # [B, R, Co, 5]
 
     def forward(self, x):
-        x = self.pool(nn.ReLU(self.conv1(x)))
-        x = self.pool(nn.ReLU(self.conv2(x)))
-        x = self.pool(nn.ReLU(self.conv3(x)))
-
-        x = x.flatten()
-
-        x = nn.ReLU(self.linear1(x))
-        x = nn.ReLU(self.linear2(x))
-        x = self.linear3(x)
+        x = self.slicer(x)
         return x
--- main.py
+++ main.py
@@ -1,24 +1,17 @@
-import cv2
+from torch import tensor
+from torch import concat
+
 import matplotlib.pyplot as plt
-import numpy as np
 
 import dataset
-from affinity import AffinityNetwork
-from torch import nn
-from torch import optim
+import affinity
 
 korte = dataset.KorteRaw('Korte')
-windowed = dataset.KorteWindowed(korte)
-
-
-
-full_image = korte[0][0]
-plt.imshow(full_image.cpu().permute(1,2,0).numpy())
-plt.show()
-
-for i in range(0, len(windowed)):
-    image, label = windowed[i]
-    if label > 0:
-        print ("Trovato! " + str(i))
-        plt.imshow(image.cpu().permute(1,2,0).numpy())
+slicer = affinity.AffinitySlicer(size=60)
+network = affinity.AffinityNetwork()
+imgs = network(korte[0][0])
+# for image in imgs:
+for x in imgs:
+    for y in x:
+        plt.imshow(y.cpu().permute(1,2,0).numpy())
         plt.show()
